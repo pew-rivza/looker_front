14b908830aac1e0028da3621a6cad501e812e3ca
pew-rivza <dz-10b@ya.ru>
2021-04-13 23:59:39 +0300

    email confirmation request added

--- src/components/authStates/EmailConfirmation.tsx
+++ src/components/authStates/EmailConfirmation.tsx
@@ -1,14 +1,17 @@
-import React, {useEffect} from "react";
+import React, {useContext, useEffect} from "react";
 import {useFormik} from "formik";
 import * as Yup from "yup";
 import {useMessage} from "../../hooks/message.hook";
 import {useHttp} from "../../hooks/http.hook";
+import {validate} from "../../utils/validation";
+import {AuthContext} from "../../context/AuthContext";
 
 type EmailConfirmationProps = {
     email: string
 }
 
 export const EmailConfirmation = ({email}: EmailConfirmationProps) => {
+    const auth = useContext(AuthContext);
     const message = useMessage();
     const {loading, request, error, clearError} = useHttp();
     const formik = useFormik({
@@ -19,36 +22,15 @@ export const EmailConfirmation = ({email}: EmailConfirmationProps) => {
         validateOnBlur: false,
         validationSchema: Yup.object({
             code: Yup.string()
-                .length(5, "Код подтверждения должен быть пятизначным")
-                .matches(/^\d{5}$/g, "код подтверждение должен состоять только из цифр")
                 .required("Код подтверждения является обязательным полем")
         }),
         onSubmit: async () => {
-            console.log("submitted!");
-
-            // try {
-            //     const data = await request("/api/auth/register", "POST", {...formik.values});
-            //     message(data.message);
-            // } catch (e) {}
+            try {
+                const data = await request("/api/auth/confirm", "POST", {...formik.values, email});
+                auth.login(data.token, data.user);
+            } catch (e) {}
         },
     });
-
-    const validateForm = async () => {
-        const validation = await formik.validateForm();
-
-        if (Object.keys(validation).length) {
-            const text = <div>
-                Форма заполнена некорректно:<br/>
-
-                {
-                    Object.values(validation).map((error, i) =>
-                        <div key={i}>- {error}</div>
-                    )
-                }
-            </div>
-            message(text)
-        }
-    };
 
     useEffect(() => {
         message(error);
@@ -58,9 +40,11 @@ export const EmailConfirmation = ({email}: EmailConfirmationProps) => {
     return (
         <>
             <form onSubmit={formik.handleSubmit}>
-                Код подтверждения для {email}:
-                <input type={"number"} {...formik.getFieldProps("code")}/>
-                <button type={"submit"} onClick={validateForm} disabled={loading}>Подтвердить</button>
+                Код подтверждения для {email}:<br/>
+                <input type={"text"} {...formik.getFieldProps("code")}/>
+                <button type={"submit"}
+                        onClick={async () => await validate(formik, message)}
+                        disabled={loading}>Подтвердить</button>
             </form>
         </>
     )
